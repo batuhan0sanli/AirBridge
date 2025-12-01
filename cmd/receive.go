55f27e6041d34647c8fd1a478fdc85e125e0b569
Batuhan Şanlı <batuhansanli@gmail.com>
2025-12-01 15:57:30 +0300

    Adds send and receive commands
    Introduces commands for secure file transfer.
    These commands initiate interactive sessions
    for sending and receiving files.

---
+++ cmd/receive.go
@@ -0,0 +1,43 @@
+/*
+Copyright © 2025 Batuhan Sanli <batuhansanli@gmail.com>
+*/
+package cmd
+
+import (
+	"AirBridge/internal/tui/receive"
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/spf13/cobra"
+)
+
+// receiveCmd represents the receive command
+var receiveCmd = &cobra.Command{
+	Use:   "receive",
+	Short: "Decrypts a file received from a sender.",
+	Long: `Starts an interactive session to decrypt a file.
+It generates a temporary key pair for this session and displays the public key.
+You can then provide the encrypted text block or the path to a .abf file.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		p := tea.NewProgram(receive.InitialModel())
+		if _, err := p.Run(); err != nil {
+			fmt.Printf("Alas, there's been an error: %v", err)
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(receiveCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// receiveCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// receiveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/send.go
@@ -0,0 +1,43 @@
+/*
+Copyright © 2025 Batuhan Sanli <batuhansanli@gmail.com>
+*/
+package cmd
+
+import (
+	"AirBridge/internal/tui/send"
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/spf13/cobra"
+)
+
+// sendCmd represents the send command
+var sendCmd = &cobra.Command{
+	Use:   "send",
+	Short: "Send a file securely.",
+	Long: `Starts an interactive session to send a file.
+It allows you to select a file, encrypt it with a recipient's public key,
+and generates the encrypted payload.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		p := tea.NewProgram(send.InitialModel(), tea.WithAltScreen())
+		if _, err := p.Run(); err != nil {
+			fmt.Printf("Alas, there's been an error: %v", err)
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(sendCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// sendCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// sendCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
